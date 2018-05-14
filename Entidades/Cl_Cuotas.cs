f11b3e9bb18254d6eb564f0b484f97d31018acd1
MonkeyWS <matidonosoliva@outlook.com>
2018-05-14 14:54:12 -0400

    avances Sergio

--- Entidades/Cl_Cuotas.cs
+++ Entidades/Cl_Cuotas.cs
@@ -11,9 +11,10 @@ namespace Entidades
     {
         public int NumeroCuota { get; set; }
         public int NumeroSocio { get; set; }
-        public int MesPago { get; set; }
+        public DateTime MesPago { get; set; }
         public DateTime  FechaPago { get; set; }
         public int Monto { get; set; }
+        public string Activo { get; set; }
 
 
         RadioTaxiEntities contexto = new RadioTaxiEntities();
@@ -89,6 +90,112 @@ namespace Entidades
             }
         }
 
-       
+        public List<Cl_Cuotas> readAll()
+        {
+            try
+            {
+                List<Cl_Cuotas> cu = new List<Cl_Cuotas>();
+                var cuotas = contexto.CUOTAS.ToList();
+                foreach (var item in cuotas)
+                {
+                    Cl_Cuotas c = new Cl_Cuotas();
+                    CommonBC.Syncronize(item, c);
+                }
+                return cu;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+
+        public List<Cl_Cuotas> generarListado(List<BibliotecaDalc.CUOTAS> socioM)
+        {
+            try
+            {
+                List<Cl_Cuotas> cu = new List<Cl_Cuotas>();
+                foreach (var item in socioM)
+                {
+                    Cl_Cuotas c = new Cl_Cuotas();
+                    CommonBC.Syncronize(item, c);
+                    cu.Add(c);
+                }
+                return cu;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        public List<Cl_Cuotas> readBymes()
+        {
+            try
+            {
+                var cuotas = from cu in contexto.CUOTAS
+                             where cu.MESPAGO == MesPago
+                             select cu;
+                return generarListado(cuotas.ToList());
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        public List<Cl_Cuotas> readEntreMeses()
+        {
+            try
+            {
+                var cuotas = from cu in contexto.CUOTAS
+                             where ((DateTime)cu.MESPAGO).ToString("MM").Equals(MesPago.ToString("MM"))
+                             select cu;
+                return generarListado(cuotas.ToList());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Cl_Cuotas> readPorSocio()
+        {
+            try
+            {
+                var cuotas = from cu in contexto.CUOTAS
+                             where cu.FK_NUMEROSOCIO == NumeroSocio
+                             select cu;
+
+                return generarListado(cuotas.ToList());
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        public List<Cl_Cuotas> readByMarca()
+        {
+            try
+            {
+                var cuotas = from cu in contexto.CUOTAS
+                             join so in contexto.SOCIOS
+                             on cu.FK_NUMEROSOCIO equals so.NUMEROSOCIO
+                             where so.MARCA.Equals((new Cl_Socios()).Marca)
+                             select cu;
+                return generarListado(cuotas.ToList());
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
     }
 }
